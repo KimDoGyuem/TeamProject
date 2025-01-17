b7360f1b6ee1e5b65b8c8f43b744b747cce63e2c
KimDoGyuem <dkdiskal357@gmail.com>
2025-01-17 19:08:17 +0900

    Add files via upload

---
+++ Project01/TeamProject1.17(update)/nsd-spring/src/main/java/com/project/mapper/PjBoardMapper.java
@@ -0,0 +1,64 @@
+package com.project.mapper;
+
+import java.util.ArrayList;
+
+import com.project.board.dto.CompanyBoardCategoryDto;
+import com.project.board.dto.CompanyBoardCommentDto;
+import com.project.board.dto.CompanyBoardDto;
+import com.project.board.dto.SearchDto;
+
+public interface PjBoardMapper {
+
+	public void write(CompanyBoardDto c);
+
+	public ArrayList<CompanyBoardDto> getPostList(String category);
+
+	public ArrayList<CompanyBoardDto> getSaerchPostList(SearchDto s);
+
+	public CompanyBoardDto read(String no);
+
+	public void hitsUp(String no);
+
+	public void commentsWrite(CompanyBoardCommentDto c);
+
+	public ArrayList<CompanyBoardCommentDto> commentsRead(String no);
+
+	public void deletePost(String no);
+
+	public void deleteComment(String no);
+
+	public void modifyPost(CompanyBoardDto c);
+
+	public ArrayList<CompanyBoardDto> getAllPostList();
+
+	public ArrayList<CompanyBoardDto> getCategoryAllPostList(String category);
+
+	public ArrayList<CompanyBoardDto> getDelPostList();
+
+	public ArrayList<CompanyBoardDto> getCategoryDelPostList(String category);
+
+	public ArrayList<CompanyBoardDto> getSearchAllPostList(SearchDto s);
+
+	public ArrayList<CompanyBoardDto> getCategorySearchAllPostList(SearchDto s);
+
+	public ArrayList<CompanyBoardDto> getSearchDelPostList(SearchDto s);
+
+	public ArrayList<CompanyBoardDto> getCategorySearchDelPostList(SearchDto s);
+
+	public void recoverPost(Integer postNo);
+
+	public void dataBaseDelPost(Integer postNo);
+
+	public void addCategory(String name);
+
+	public ArrayList<CompanyBoardCategoryDto> categoryList();
+
+	public void deleteCategory(String name);
+
+	public void deleteCategoryPosts(String name);
+
+	/* 게시판 메인 페이지 상단 공지 글, 인기 글 */
+	public ArrayList<CompanyBoardDto> selectTopPostsByCategory(int limit);
+
+	public ArrayList<CompanyBoardDto> selectTopPostsByHits(int limit);
+}
