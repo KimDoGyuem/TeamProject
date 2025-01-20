bccd96d1cb2b3fb5d5bf3c911af322d021bc6ce7
KimDoGyuem <dkdiskal357@gmail.com>
2025-01-20 21:09:29 +0900

    Add files via upload

---
+++ Project01/2025.01.20Work/nsd(teampj).sql
@@ -0,0 +1,144 @@
+use my_cat;
+show tables;
+
+create table company_member(
+	name varchar(20) not null,
+	company_id varchar(20) primary key,
+    password varchar(20) not null,
+    position varchar(20),
+    p_rank int default 2,
+    department varchar(20) default '',
+    profile_photo varchar(5) DEFAULT '1'
+);
+select * from company_member order by p_rank asc;
+
+insert into company_member(name,company_id,password,position,p_rank) values('김사원','w1','11','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('이사원','w2','22','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('박사원','w3','33','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('최사원','w4','44','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('정사원','w5','55','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('강사원','w6','66','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('조사원','w7','77','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('윤사원','w8','88','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('장사원','w9','99','사원',2);
+insert into company_member(name,company_id,password,position,p_rank) values('왕사장','boss','00','사장',0);
+
+##사원 비밀번호 자동 입력 트리거 
+DELIMITER $$
+CREATE TRIGGER set_pw_auto_generation_trigger 
+BEFORE INSERT ON company_member
+FOR EACH ROW
+BEGIN
+    IF NEW.password IS NULL THEN					## password 는 변수명임..
+        -- password에 company_id 값과 추가 문자열 'a'를 결합하여 설정
+        SET NEW.password = CONCAT(NEW.company_id, '00');
+    END IF;
+END$$
+DELIMITER ;
+
+insert into company_member(name,company_id,position,p_rank) values('김씨','000','사원',2);	##테스트 insert 문
+
+DROP TRIGGER IF EXISTS set_pw_auto_generation_trigger;		## 트리거 삭제문
+SHOW TRIGGERS;		##트리거 목록 확인
+
+
+create table project_list(
+	no int primary key auto_increment,
+	project_name varchar(50) ,
+	project_content varchar(50),
+    project_period text,
+    is_finished boolean default false
+);
+select * from project_list;
+
+ALTER TABLE project_list ADD COLUMN is_finished boolean default false;
+update project_list set is_finished = false where no = 3;
+
+create table project_member_list(
+	company_id varchar(50),
+    project_number int
+);
+select * from project_member_list;
+
+create table department_list(
+	no int primary key auto_increment,
+	department_name varchar(50),
+    team_leader varchar(50) default '미정'
+);
+select * from department_list;
+
+drop table company_member;
+truncate department_list;
+
+##여기부터 게시판
+create table company_board(
+	cb_no int primary key auto_increment, ##글번호 
+    company_id varchar(50),	##작성자id
+	company_name varchar(50),	##작성자 이름
+    cb_title varchar(255),		##글제목
+    cb_text text,			##글내용
+    cb_category varchar(50),	##카테고리
+    cb_datetime DATETIME DEFAULT now(),	##작성 날짜
+    cb_modify_datetime DATETIME DEFAULT now(), ##수정 날짜
+    cb_hits int default 0,	##조회수
+    cb_is_deleted BOOLEAN DEFAULT FALSE ##삭제 로직
+);
+select * from company_board order by cb_no desc;
+SELECT * FROM company_board WHERE cb_is_deleted = FALSE AND cb_category != '공지' AND cb_hits > 3 ORDER BY cb_no DESC LIMIT 5;
+
+INSERT INTO company_board (company_id, company_name, cb_title, cb_text, cb_category) VALUES ('boss', '왕사장', '아재개그', '이것은 뭘까요?', '자유');
+INSERT INTO company_board (company_id, company_name, cb_title, cb_text, cb_category) VALUES ('w1', '김사원', '영화 감상', '오징어 게임2 재미있어요.', '자유');
+INSERT INTO company_board (company_id, company_name, cb_title, cb_text, cb_category) VALUES ('w2', '이사원', '주식 토크', '요즘 주식이 올랐어요.', '자유');
+INSERT INTO company_board (company_id, company_name, cb_title, cb_text, cb_category) VALUES ('boss', '왕사장', '회사 내 분리수거 철저히 하기', '이것은 뭘까요?', '공지');
+UPDATE company_board SET cb_hits = cb_hits + 30 WHERE cb_no = 55;
+
+create table company_board_comment(
+	comment_no int primary key auto_increment,
+	cb_comment_no int,		##해당 글번호
+    company_id varchar(50),	##댓글 작성자id
+	company_name varchar(50),	##작성자 이름
+    cb_comment_text text,	##댓글 내용
+    cb_comment_date DATETIME DEFAULT now(),	##댓글 작성 날짜
+    cb_comment_is_deleted BOOLEAN DEFAULT FALSE,	##삭제 로직
+    FOREIGN KEY (cb_comment_no) REFERENCES company_board(cb_no) ON DELETE CASCADE ##메인 게시글 삭제 시 댓글 자동 삭제 로직
+);
+select * from company_board_comment;
+
+## 카테고리 관리 로직
+create table company_board_category(
+	cb_category_name varchar(50)
+);
+select * from company_board_category;
+insert into company_board_category(cb_category_name) values('공지'), ('자유');
+
+drop table company_board;
+truncate company_board;
+
+## 이슈 리스트 테이블
+drop table project_issue_list;
+create table project_issue_list(
+	issue_no int primary key auto_increment,
+    issue_project_no int not null,
+    issue_title varchar(50),
+    issue_details text,
+    issue_label varchar(20),
+    issue_author varchar(20),
+    issue_date datetime default now(),
+    issue_is_closed boolean default false,
+    issue_closedDate datetime
+);
+select * from project_issue_list;
+insert into project_issue_list (issue_project_no, issue_title, issue_details, issue_label, issue_author) values(3, '이슈존재테스트', '이슈내용테스트', 'bug', '박사원');
+select count(*) from project_issue_list where issue_project_no = 3;
+
+## 이슈 코멘트 테이블
+drop table project_issue_comment;
+create table project_issue_comment(
+	comment_no int primary key auto_increment,
+    comment_issue_no int not null,
+    comment_text text,
+    comment_author varchar(20),
+    comment_date datetime default now()
+);
+insert into project_issue_comment (comment_issue_no, comment_text, comment_author) values(1, '이슈코멘트테스트', '이사원');
+select * from project_issue_comment;
---
+++ Project01/2025.01.20Work/src/board/BoardGetList.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react';
+import './Board.css';
+import axios from 'axios';
+import highlightText from '../utilPages/HighLightText';
+import Pagenation from '../utilPages/Pagination';
+
+function BoardGetList({ category, page, readPage, loginRank }) {    
+
+    const [posts, setPost] = useState([]);
+    const [searchTag, setSearchTag] = useState('cb_title');
+    const [tempSearchTag, setTempSearchTag] = useState('cb_title');
+    const [searchWord, setSearchWord] = useState('');
+    const [tempSearchWord, setTempSearchWord] = useState('');
+    //조회수 정렬 상태 관리
+    const [isHitsSorted, setIsHitsSorted] = useState(false);
+
+    const shouldShowButton = !(category === '공지' && loginRank > 0);  
+    
+    useEffect(() => {
+        axiosGetPostList();
+    }, [searchWord, searchTag]);
+
+    function axiosGetPostList() {
+        axios.get(`http://localhost:8080/spring/companyBoard/getPostList?category=${category}&searchTag=${searchTag}&searchWord=${searchWord}`)
+            .then((response) => {
+                setPost(response.data);
+                setCurrentPage(1);
+                setCurrentBlock(1);
+            })
+            .catch(error => {
+                console.error('에러!', error);
+            })
+    }
+
+    function read(no) {
+        axios.get(`http://localhost:8080/spring/companyBoard/hitsUp?no=${no}`)
+            .then(() => {
+                readPage('board_read', no)
+            })
+            .catch(error => {
+                console.error('에러!', error);
+            })
+    }
+
+    function search(e) {
+        e.preventDefault();
+        setSearchWord(tempSearchWord);
+        setSearchTag(tempSearchTag);
+    }
+
+    //조회수 정렬 함수
+    function handleSortByHits() {
+        const sortedPosts = [...posts].sort((a, b) => //sort는 배열 정렬 메서드
+            (isHitsSorted ? b.cb_hits - a.cb_hits : a.cb_hits - b.cb_hits));
+        setPost(sortedPosts);
+        setIsHitsSorted(!isHitsSorted); //정렬 상태 반전
+    }
+
+    //날짜 YYYY-MM-DD 형식으로 변환
+    function formatDate(dateString) {
+        const date = new Date(dateString); // 문자열을 Date 객체로 변환
+        return date.toLocaleDateString('en-CA'); // YYYY-MM-DD 형식으로 반환
+    }
+
+    //페이징 블럭 변수 모음 ~~~~~
+    const [currentPage, setCurrentPage] = useState(1);  //현재 페이지 번호
+    const [currentBlock, setCurrentBlock] = useState(1); // 현재 블록 번호
+    const postsPerPage = 5; // 페이지당 게시물 수
+    const blockSize = 3; // 한 블록에 표시할 페이지 수
+    const indexOfLastPost = currentPage * postsPerPage; //현재 블록 기준 페이지 끝번호 계산식(시작 인덱스)
+    const indexOfFirstPost = indexOfLastPost - postsPerPage; //현재 블록 기준 페이지 첫번호 계산식(종료 인덱스)
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost); //slice함수: 기존 배열에서 특정 부분을 추출하여 새 배열을 반환. 원본 배열은 수정되지 않음 
+    const totalPages = Math.ceil(posts.length / postsPerPage);  //총 페이지 수 계산
+    //~~~~~여기까지
+
+    return (
+        <div id='board_get_list_page'>
+            <div id='list_box'>
+                <h3>{category} 게시판</h3>    
+                <table style={{ width: '1270px' }}>
+                    <tbody>
+                        <tr>
+                            <td style={{ width: '10%', height: '30px' }}>글번호</td>
+                            <td style={{ width: '55%' }}>글제목</td>
+                            <td style={{ width: '14%' }}>작성자</td>
+                            <td style={{ width: '14%' }}>작성일</td>
+                            <td style={{ width: '7%' }}>조회수<button onClick={handleSortByHits} style={{ width: '25px', paddingLeft: '2px'}}>
+                                {isHitsSorted ? '🔼' : '🔽'}
+                            </button></td>
+                        </tr>
+                        {currentPosts.map((p, i) =>
+                            <tr key={i}>
+                                <td style={{ width: '10%', height: '30px' }}>{p.cb_no}</td>
+                                <td id='post_title_link' style={{ width: '55%' }} onClick={() => read(p.cb_no)}>{highlightText(p.cb_title, searchWord)}</td>
+                                <td style={{ width: '14%' }}>{p.company_name}</td>
+                                {p.formattedDate === p.formattedModifyDate ? 
+                                <td style={{ width: '14%' }}>{formatDate(p.formattedDate)}</td> : 
+                                <td style={{ width: '14%' }}>{formatDate(p.formattedModifyDate)} *수정됨</td>
+                                }
+                                <td style={{ width: '7%' }}>{p.cb_hits}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                <hr />
+
+                {/* 페이징 블럭 ~~~~~*/}
+                <Pagenation currentPage={currentPage} totalPages={totalPages} blockSize={blockSize} onPageChange={setCurrentPage} currentBlock={currentBlock} setCurrentBlock={setCurrentBlock} />
+                {/* ~~~~~여기까지 */}
+
+                <hr />
+                <button id='main_button' onClick={() => page('board')}>메인으로</button> &nbsp;
+                {/* <button id='main_button' onClick={() => page('board_write')}>글쓰기</button>     */}
+                {shouldShowButton && <button id='main_button' onClick={() => page('board_write')}>글쓰기</button>} 
+                <hr />
+                <select onChange={(e) => setTempSearchTag(e.target.value)}>
+                    <option value='cb_title'>글제목</option>
+                    <option value='cb_text'>글내용</option>
+                    <option value='company_name'>작성자</option>
+                </select>
+                <form onSubmit={search}>
+                    <input value={tempSearchWord} onChange={(e) => setTempSearchWord(e.target.value)} placeholder='검색어를 입력 해주세요'></input>
+                    <button type='submit'>검색</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default BoardGetList;
