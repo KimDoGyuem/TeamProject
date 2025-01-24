df333ee54a135f1ace1f6b16e9f3fcde0b9d9be1
KimDoGyuem <dkdiskal357@gmail.com>
2025-01-24 17:28:08 +0900

    Add files via upload

---
+++ Project01/2025.01.24Work/nsd-spring/src/main/java/com/project/board/dto/CompanyBoardCommentDto.java
@@ -0,0 +1,21 @@
+package com.project.board.dto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import lombok.Data;
+
+@Data
+public class CompanyBoardCommentDto {
+	private int comment_no;
+	private int cb_comment_no;
+	private String company_id;
+	private String company_name;
+	private String cb_comment_text;
+	private String formattedDate;
+	private boolean cb_comment_is_deleted;
+
+	public void setCb_comment_date(LocalDateTime cb_comment_date) {
+        this.formattedDate = cb_comment_date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+}
